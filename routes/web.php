48cea1054352b2a48abff89fa5d778957ca02cdd
Enzo-Joseph <enzojsphperso@gmail.com>
2024-10-08 18:18:18 +0700

    Added layout component and question view
    The layout component serves as a layout for the views.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/questions', function () {
+    return view('questions', [
+        'questions' => App\Models\Question::all(),
+    ]);
+});
